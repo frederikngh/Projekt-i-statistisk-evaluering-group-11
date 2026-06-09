b241037f30d312cf3750dc9cb7deddcb4df262a2
Frederik Naervig <frederikngh@gmail.com>
2026-06-09 12:42:12 +0200

    gemma code

--- simple_gemma.py
+++ simple_gemma.py
@@ -3,6 +3,6 @@ from transformers import pipeline
 
 gemma = pipeline("image-text-to-text", model="google/gemma-4-E4B-it", dtype="auto", device="mps")
 
-PROMPT = "What is the capital of Denmark? Answer in one sentence."
+PROMPT = "Hvem er Frederik Naervig Gerlach-Hansen?"
 out = gemma(text=[{"role": "user", "content": [{"type": "text", "text": PROMPT}]}], max_new_tokens=200)
 print(out[0]["generated_text"][-1]["content"])
